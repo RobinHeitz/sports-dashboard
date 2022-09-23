d51243e3e58d4d6447755652688ace5460b02b5b
Robin Heitz <blk_rh@wzl.rwth-aachen.de>
2022-09-23 12:14:17 +0200

    Refactored table rendering; Dashboard.py gets tables and does not need to know anything about data structure.

--- definitions.py
+++ definitions.py
@@ -13,9 +13,16 @@ class SportsType(Enum):
     handball = auto()
 
 
+
+
 class GameItem(Protocol):
     def output_table_row(self) -> Tuple[str]:
-        pass
+        """Returns tuple of attributes of this instance. Number of items within tuple need to match the number of items from get_columns()."""
+
+    def get_columns():
+        """Returns list of dictionarys with attributes that define how the column-headers should be displayed."""
+
+
 
 @dataclass
 class DashboardItem:
@@ -24,6 +31,8 @@ class DashboardItem:
     sports_type: SportsType
     dashboard_type: DashboardType
     game_items: List[GameItem]
+
+
 
 
 @dataclass
@@ -35,13 +44,26 @@ class GameResult:
     home_goals: int
     opposing_goals: int
 
+    @classmethod
+    def get_columns(cls) -> List[dict]:
+        """Returns list of dictionarys with attributes that define how the column-headers should be displayed."""
+        return [
+            dict(header="Date", justify="center", style="cyan"),
+            dict(header="Home Team", justify="center", style="blue"),
+            dict(header="Opposing Team", justify="center", style="yellow"),
+            dict(header="Result", justify="center", style="red"),
+        ]
+
     def output_table_row(self) -> Tuple[str]:
+        """Returns tuple of attributes of this instance. Number of items within tuple need to match the number of items from get_columns()."""
         return (
             self.date.strftime("%a, %d. %b %y - %H:%M"),
             self.home_team,
             self.opposing_team, 
             f"{self.home_goals} : {self.opposing_goals}"
             )
+
+
 
 @dataclass
 class CompetitionTableSpot:
@@ -56,13 +78,25 @@ class CompetitionTableSpot:
     def points(self) -> int:
         return self.victories * 2 + self.draws
 
+    @classmethod
+    def get_columns(cls) -> List[dict]:
+        """Returns list of dictionarys with attributes that define how the column-headers should be displayed."""
+        return [
+            dict(header="Team", justify="left", style="cyan"),
+            dict(header="Victories", justify="center", style="green"),
+            dict(header="Draws", justify="center", style="yellow"),
+            dict(header="Defeats", justify="center", style="red"),
+            dict(header="Points", justify="center", style="blue"),
+        ]
+
     def output_table_row(self) -> Tuple[str]:
+        """Returns tuple of attributes of this instance. Number of items within tuple need to match the number of items from get_columns()."""
         return (
             self.team, 
             str(self.victories),
             str(self.draws),
             str(self.defeats),
-            str(self.points)
+            str(self.points),
         )
 
     def __lt__(self, other):
--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
-from definitions import DashboardItem, DashboardType, GameItem, GameResult, SportsType
+from definitions import CompetitionTableSpot, DashboardItem, DashboardType, GameItem, GameResult, SportsType
 from games_factory import handball_bl_gameday_item, handball_bl_placement_table_item
+
+from table_creation import _create_table, create_tables
 
 from datetime import datetime
 
@@ -52,4 +54,22 @@ class TestGamesFactory(TestCase):
             len(render), 4
         )
 
-    
+class TestTableCreation(TestCase):
+
+    def test_create_table(self):
+        ...
+        titel = "Test Title"
+        games = [
+            CompetitionTableSpot(team="Vfl Gummersbach", victories=1, draws=2, defeats=3),
+            CompetitionTableSpot(team="THW Kiel", victories=3, draws=1, defeats=0),
+            CompetitionTableSpot(team="SG Flensburg-Handewitt", victories=3, draws=1, defeats=0),
+            CompetitionTableSpot(team="SC Magdeburg", victories=5, draws=1, defeats=0),
+        ]
+
+        item = DashboardItem(title=titel, sports_type=SportsType.handball, dashboard_type=DashboardType.placement_table, game_items=games)
+        try:
+            table = _create_table(item)
+        except Exception:
+            self.fail("Exception raised!")
+
+
