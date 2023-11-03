27e2bdd2eb19a2224e73f102cebdf4baa24eabc5
Heitz Robin <heitz@igmr.rwth-aachen.de>
2023-11-03 22:38:26 +0100

    reworked args

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -11,20 +11,28 @@ from handball.parse_bundesliga import process_scheduled_gamedays, parse_date_of_
 import yaml
 
 
-def main(testing=False, save_html=False):
+def main(**kwargs):
     
     with open("config.yaml", "r") as f:
         config = yaml.safe_load(f)
+
+    if kwargs["bl_off"] == False:
+        print("=== Start processing bundesliga")
+        content_hbl = get_website_content(config = config["handball_bl"], **kwargs)
+        scheduled_gamedays_bundesliga = process_handball_bundesliga(content_hbl)
+        for i in scheduled_gamedays_bundesliga:
+            print(i)
+
+    if kwargs["cl_off"] == False:
+        print("=== Start processing champions league")
+        # content_cl = get_website_content(config = config["handball_cl"], **args)
+        # items = process_handball_championsleague(content_cl)
     
-    content_hbl = get_website_content(testing, save_html, config["handball_bl"])
-
-    scheduled_gamedays_bundesliga = process_handball_bundesliga(content_hbl)
-    for i in scheduled_gamedays_bundesliga:
-        print(i)
 
 
 
-def get_website_content(testing, save_html, config):
+
+def get_website_content(testing, save_html, config, **kwargs):
     offline_filename = config["offline_filename"]
     if testing is True:
 
@@ -54,6 +62,11 @@ def process_handball_bundesliga(content):
 
     scheduled_gamedays = parse_date_of_matches(paired_matches_dict)
     return scheduled_gamedays
+
+
+
+def process_handball_championsleague(content):
+    ...
     
 
 
@@ -62,6 +75,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Handball Table Scraper", description="Scrapes gamedays's data.")
     parser.add_argument("-t", "--test", action="store_true", help="Uses old website data for faster development of parsing (instead of scraping).")
     parser.add_argument("-s", "--save_html", action="store_true", help="Save html content in a .txt file. Has no effect with -t.")
+    parser.add_argument("-b", "--bl_off", action="store_true", help="Turns off bundesliga parsing.")
+    parser.add_argument("-c", "--cl_off", action="store_true", help="Turns off champions league parsing.")
 
     args = parser.parse_args()
-    main(testing = args.test, save_html=args.save_html)
+    main(testing = args.test, save_html=args.save_html, bl_off = args.bl_off, cl_off = args.cl_off)
